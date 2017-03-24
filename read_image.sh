1d00fb660da42a42509e2250d668f52ff19d7804
Aymeric VIAL-GRELIER <aymvial@gmail.com>
2017-03-24 12:09:38 +0100

    update script.sh

--- read_image.sh
+++ read_image.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 [ -e "result.bin" ] && rm result.bin
-if [ ! -f "$file" ] ; then
-    # if not create the file
-fi
 while IFS='' read -r line || [[ -n "$line" ]]; do
     ./read_image $line
 done < "$1"
