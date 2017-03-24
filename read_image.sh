5533b57860350db70da0a19839a7b10c1ddd49c2
Aymeric VIAL-GRELIER <aymvial@gmail.com>
2017-03-24 12:06:02 +0100

    update script.sh

--- read_image.sh
+++ read_image.sh
@@ -2,7 +2,6 @@
 [ -e "result.bin" ] && rm result.bin
 if [ ! -f "$file" ] ; then
     # if not create the file
-    echo "pas de fichier présent"
 fi
 while IFS='' read -r line || [[ -n "$line" ]]; do
     ./read_image $line
